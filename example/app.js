1d8e725ac8eca7d41a08704293cee9425c45f368
Batuhan GÜNDOĞDU <gundogdu.batuhan61@gmail.com>
2018-03-23 16:28:02 +0300

    update

--- example/app.js
+++ example/app.js
@@ -1,9 +1,24 @@
 var thinkgear = require('../lib');
-
+var request = require('request');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var ary = [];
 var client = thinkgear.createClient();
+var x;
+client.on('data', function(data) {
 
-client.on('data',function(data){
-	console.log((new Date).toISOString() + ': ' + JSON.stringify(data));
+    if (data.poorSignalLevel === 0) {
+        x = data;
+        console.log(data);
+
+        app.get('/', function(req, res) {
+            res.send(x);
+        });
+    } else {
+        console.log("Poor Signal");
+    }
 });
 
+app.listen(3000);
 client.connect();
